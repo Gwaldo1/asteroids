b5bc23e5023a8730ea7891986f7a181529ef7c2d
gwaldo1 <aliefendioglu.ates@gmail.com>
2024-08-29 11:50:21 +0300

    added motion to player and grouped game objects

--- main.py
+++ main.py
@@ -7,18 +7,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
     
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-
+        for obj in updateable:
+            obj.update(dt)
+        
         screen.fill("black") 
-        player.draw(screen)
+
+        for b in drawable:
+            b.draw(screen)
+
         pygame.display.flip()
 
         # Limit the framerate to 60 FPS
